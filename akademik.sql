204d1ccd15895a11d0d2bb48464826eb753cc15a
alvianoutsider <48824172+alvianoutsider@users.noreply.github.com>
2021-11-30 21:33:52 +0700

    Add files via upload

--- akademik.sql
+++ akademik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 12, 2021 at 05:01 PM
+-- Generation Time: Nov 30, 2021 at 03:15 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.8
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `akademik`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `khs`
+--
+
+CREATE TABLE `khs` (
+  `nim` varchar(5) NOT NULL,
+  `kodeMK` varchar(5) NOT NULL,
+  `nilai` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `khs`
+--
+
+INSERT INTO `khs` (`nim`, `kodeMK`, `nilai`) VALUES
+('MH01', '008', 'B'),
+('MH02', '666', 'B+'),
+('MH03', '111', 'A-'),
+('MH04', '888', 'A');
 
 -- --------------------------------------------------------
 
@@ -43,17 +65,84 @@ INSERT INTO `mahasiswa` (`nim`, `nama`, `jkel`, `alamat`, `tgllhr`) VALUES
 ('MH01', 'Siti Aminah', 'P', 'SOLO', '1995-10-01'),
 ('MH02', 'Rita', 'P', 'SOLO', '1999-01-01'),
 ('MH03', 'Amirudin', 'L', 'SEMARANG', '1998-08-11'),
-('MH04', 'Siti Maryam', 'P', 'JAKARTA', '1995-04-15');
+('MH04', 'Siti Maryam', 'P', 'JAKARTA', '1995-04-15'),
+('MH05', 'Bambang', 'L', 'JOGJA', '1998-09-09'),
+('MH06', 'Ega', 'L', 'SLEMAN', '1999-12-01'),
+('MH07', 'Nia', 'P', 'Bantul', '1995-03-20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matakuliah`
+--
+
+CREATE TABLE `matakuliah` (
+  `kodeMK` varchar(5) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `sks` int(2) NOT NULL,
+  `sem` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `matakuliah`
+--
+
+INSERT INTO `matakuliah` (`kodeMK`, `nama`, `sks`, `sem`) VALUES
+('008', 'Metopen', 3, 6),
+('111', 'Pemrograman Web Dinamis', 3, 5),
+('666', 'Grafika Komputer', 2, 4),
+('888', 'TBQ', 0, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `nama_lengkap` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `level` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `password`, `nama_lengkap`, `email`, `level`) VALUES
+('bambang33', 'df50be5a451697ce284fa72b098eb5fe', 'bambang suparman', 'bambangsuparman33@test.tesr', ''),
+('nanda32', 'nanda32', 'nanda bagus', 'nandabagus32@test.test', '1');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `khs`
+--
+ALTER TABLE `khs`
+  ADD UNIQUE KEY `nim` (`nim`),
+  ADD UNIQUE KEY `kodeMK` (`kodeMK`);
+
+--
 -- Indexes for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
   ADD PRIMARY KEY (`nim`);
+
+--
+-- Indexes for table `matakuliah`
+--
+ALTER TABLE `matakuliah`
+  ADD PRIMARY KEY (`kodeMK`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
